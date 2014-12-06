f77425ef579c9c832e16ef2594b60766f9527c1f
yao <yao@yaotekiMacBook-Pro.local>
2014-12-06 18:41:49 +0800

    merge

---
+++ Shop/ElectronicBueiness/AdapterViewController.h
@@ -0,0 +1,13 @@
+//
+//  AdapterViewController.h
+//  ElectronicBueiness
+//
+//  Created by yao on 14/12/6.
+//  Copyright (c) 2014年 yao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AdapterViewController : UIViewController
+
+@end
